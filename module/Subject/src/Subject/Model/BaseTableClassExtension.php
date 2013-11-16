9076f06eb3109bf9f1c408d35d934343c55a5b72
Greg Davies <greg@Greg-TestPC.(none)>
2013-11-16 01:56:43 +0000

    Removed superfluous comments.

--- module/Subject/src/Subject/Model/BaseTableClassExtension.php
+++ module/Subject/src/Subject/Model/BaseTableClassExtension.php
@@ -3,9 +3,6 @@ namespace Subject\Model;
 
 class BaseTableClassExtension
 {
-	/*public function __construct($sm) {
-		$this->sm = $sm;
-	}*/
 	public function exchangeArray($data) {
 		foreach($this->getArrayCopy() as $name => $junk) {
 			$this->$name = (isset($data[$name])) ? $data[$name] : null;
