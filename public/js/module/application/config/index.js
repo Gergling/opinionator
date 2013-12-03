c64808dcfec2fd74043c1a06f7fa8cb1ead0b268
Greg Davies <greg@Greg-TestPC.(none)>
2013-12-03 00:50:48 +0000

    Standardised subject list name.

--- public/js/module/application/config/index.js
+++ public/js/module/application/config/index.js
@@ -2,6 +2,6 @@ qh.getModule('application').config(['$routeProvider', function($routeProvider) {
 	$routeProvider.when('', {templateUrl: 'js/module/application/partial/dashboard.html'});
 	$routeProvider.when('/', {templateUrl: 'js/module/application/partial/dashboard.html'});
 	$routeProvider.when('/subject/:subjectId/', {templateUrl: 'js/module/application/partial/subject.html'});
-	$routeProvider.when('/list/', {templateUrl: 'js/module/application/partial/subjectList.html'});
+	$routeProvider.when('/list/', {templateUrl: 'js/module/application/partial/subject-list.html'});
 	$routeProvider.otherwise({templateUrl: 'js/module/application/partial/404.html'});
 }]);
