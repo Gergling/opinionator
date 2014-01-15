bcc2320213e11ea29c3345c3487c2a169517ccef
Greg Davies <greg@Greg-TestPC.(none)>
2014-01-15 18:23:30 +0000

    Sort improvements. Now handles loading delay better.

--- public/js/module/sort-filter/factory/sort.js
+++ public/js/module/sort-filter/factory/sort.js
@@ -2,14 +2,35 @@ qh.component('sort-filter', function(ngm, qhm) {
 	ngm.factory(qhm.getComponent('factory', 'sort').getFullName(), ["$rootScope", "$http", function($scope, $http) {
 		var obj = {
 			sorts: {},
+				
+			createCurrent: function(id, asc) {
+				return {id:id, asc:asc};
+			},
 
-			addSort: function(name, columns) {obj.sorts[name] = new obj.Sort(columns);},
-			getSort: function(name) {return obj.sorts[name];},
+			addSort: function(name, columns, current) {
+				if (!obj.sorts[name]) {
+					obj.sorts[name] = new obj.Sort(columns, current);
+				}
+				var sort = obj.getSort(name);
+				$scope.$broadcast(qhm.getComponent('factory', 'sort').getFullName()+".addSort", {
+					name: name,
+					sort: sort,
+				});
 
-			Sort: function(columns) {
+				return sort;
+			},
+			getSort: function(name) {
+				if (obj.sorts[name]) {
+					return obj.sorts[name];
+				} else {
+					throw "sort-filter.factory.sort.getSort: Unknown sort '"+name+"'.";
+				}
+			},
+
+			Sort: function(columns, current) {
 				var localScope = this;
 				this.columns = columns;
-				this.current = [];
+				this.current = current || [];
 
 				// Needs to be able to add to current columns, remove from current columns and modify order.
 				this.add = function(id) {
@@ -20,15 +41,25 @@ qh.component('sort-filter', function(ngm, qhm) {
 					this.current.splice(this.current.indexOf(id), 1);
 					this.update();
 				};
+				this.updateCurrentIndex = function() {
+					localScope.current.index
+				};
+				this.getCurrent = function(id) {
+					var ret = {};
+					var x = angular.forEach(localScope.current, function(current, currentPosition) {
+						if (current.id==id) {ret = {current: current, currentPosition: currentPosition};return;}
+					});
+					return ret;
+				};
 				this.update = function() {
 					localScope.unusedColumns = [];
 					localScope.sortingByColumns = [];
 
 					angular.forEach(localScope.columns, function(label, id) {
-						var currentPosition = localScope.current.indexOf(id);
+						var current = localScope.getCurrent(id);
 						var column = {id: id, label: label};
-						if (currentPosition>-1) {
-							localScope.sortingByColumns[currentPosition] = column;
+						if (current.currentPosition>-1) {
+							localScope.sortingByColumns[current.currentPosition] = $.extend(column, {asc:current.current.asc});
 						} else {
 							localScope.unusedColumns.push(column);
 						}
