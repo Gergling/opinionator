9f02ad843b3ab6ea75cfbed461db526439baaea1
Greg Davies <greg@Greg-TestPC.(none)>
2014-01-15 18:24:34 +0000

    Subject improvement to accept sorting changes.

--- public/js/module/subject/factory/list.js
+++ public/js/module/subject/factory/list.js
@@ -1,27 +1,37 @@
-qh.getModule('subject').factory("subject.factory.list", ["$rootScope", "$http", function($scope, $http) {
-	var obj = {
-		fetch: function(params) {
-			return $http({
-				method: 'GET',
-				url: "/subject",
-				params: params,
-			}).success(function (response) {
-				obj.list = response.data;
-			});
-		},
-		list: [],
-		validateParams: function(inputParams) {
-			var params = {
-				offset: 0,
-				limit: 5,
-				whereString: "",
-				orderString: "sum ASC",
-			};
-			$.each(params, function(name, value) {
-				params[name] = inputParams[name] || params[name];
-			});
-			return params;
-		},
-	};
-	return obj;
-}]);
+qh.component('subject', function(ngm, qhm) {
+	ngm.factory(qhm.getComponent('factory', 'list').getFullName(), ["$rootScope", "$http", "$q", function($scope, $http, $q) {
+		var obj = {
+			fetch: function(params, forceUpdate) {
+				var promise = $q.defer().promise;
+				var updated = false;
+				if (Object.keys(obj.list).length || forceUpdate) {
+					promise = $http({
+						method: 'GET',
+						url: "/subject",
+						params: params,
+					}).success(function (response) {
+						var key = JSON.stringify(jQuery.extend(true, {}, response.searchParams));
+						obj.list.all[key] = response;
+						obj.list.last = response;
+						updated = true;
+					});
+				}
+				return promise;
+			},
+			list: {all: {}, last: {}},
+			validateParams: function(inputParams) {
+				var params = {
+					offset: 0,
+					limit: 5,
+					whereString: "",
+					orderString: "sum ASC",
+				};
+				$.each(params, function(name, value) {
+					params[name] = inputParams[name] || params[name];
+				});
+				return params;
+			},
+		};
+		return obj;
+	}]);
+});
